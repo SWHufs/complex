93daff10b2f77c9fbf4f75305d617c22c8b487cf
Jeongyoon Yang <backyang0716@gmail.com>
2022-07-05 15:45:19 +0900

    op_sub branch added

--- complex.py
+++ complex.py
@@ -29,19 +29,14 @@ class Complex:
         c.re = self.re + c1.re
         c.im = self.im + c1.im
         return c
-
-c1 = Complex(1, 2)
-print(c1)
-
-c2 = Complex(2, 3)
-print(c1.add(c2))
-    
-c1 = Complex(1, 2)
-print(c1)
-c2 = Complex(2,3)
-print(c2.subtract(c1))
         
+    def __sub__(self, c):
+        return self.subtract(c)
+
 c1 = Complex(1, 2)
 print(c1)
-c2 = Complex(2,3)
+c2 = Complex(2, 3)
+print(c2-c1)
+print(c2.subtract(c1))
 print(c1.multiply(c2))
+        
